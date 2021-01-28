a0123a32368411cf7550a014e1f894532b679e9e
ch-luan2015 <ch.luan2015@gmail.com>
2021-01-28 21:40:06 +0700

    Fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import { render } from "react-dom";
 import App from "./App";
 
+import { createStore, applyMiddleware, Store } from "redux";
+import reducer from "./store/reducer";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
-import { store } from "./store";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+const store: Store<ArticleState, ArticleAction> & {
+  dispatch: DispatchType;
+} = createStore(reducer, applyMiddleware(thunk));
+
+const rootElement = document.getElementById("root");
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
