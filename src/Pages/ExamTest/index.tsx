3381fada48e2ad20ab832dc3cebac1fa5063af64
ch-luan2015 <ch.luan2015@gmail.com>
2021-02-13 03:04:00 +0700

    new

--- src/Pages/ExamTest/index.tsx
+++ src/Pages/ExamTest/index.tsx
@@ -1,23 +1,24 @@
 import React, { useContext } from "react";
 import mathData from "../../data/Toan.json";
+import deThat from "../../data/HocMai.json";
 import QuestionCard from "./QuestionCard";
 import ChooseTable from "./ChooseTable";
 import { Exam } from "resource/model/question";
 import { Flex, Box, Spacer, Button, Text } from "@chakra-ui/react";
-import { StoreContext } from "ultis/store";
 import Example from "./Example";
 
 function ExamTest() {
   const exam = mathData as Exam;
-  const { count }: any = useContext(StoreContext);
-  console.log({ count });
 
-  const inCount = () => count.setCount(count.count + 1);
+  var { title, subtitle, questions } = deThat;
+  console.log("deThat", deThat);
+  console.log("title", title);
+  console.log("subtitle", subtitle);
+  console.log("questions", questions);
+
   return (
     <>
-      <Button onClick={inCount}>+1</Button>
-      <Text>{count.count}</Text>
-      <Flex bg="gray.300" h="100vh" wrap="wrap" direction="row" justify="flex-start" align="flex-start">
+      {/* <Flex bg="gray.300" h="100vh" wrap="wrap" direction="row" justify="flex-start" align="flex-start">
         <Flex flex="1 0 0">
           <Spacer />
           <ChooseTable index={exam.questions.length} />
@@ -33,9 +34,13 @@ function ExamTest() {
 
           <Example />
         </Box>
-      </Flex>
+      </Flex> */}
+      <Text>{title}</Text>
+      <Text>{subtitle}</Text>
+      <Text>{questions.map((item) => item.answers.map((answers1) => answers1.content))}</Text>
     </>
   );
 }
 
 export default ExamTest;
+//Bo mot react-markdown vao de render  answers1.content
